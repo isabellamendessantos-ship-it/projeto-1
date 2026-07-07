c3499179c8bc6449c5330d8452b218a1a17c89d2
isabellamendessantos-ship-it <isabella.mendes.santos@escola.pr.gov.br>
2026-07-07 10:37:03 -0300

    f

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ botoes.forEach(function (botao){
 
     function botaoClicado(){
         console.log("botao clicado")
-        ler texto = botao.querySelector("span")
+        let texto = botao.querySelector("span")
+        texto.textContent++
     }
 })
